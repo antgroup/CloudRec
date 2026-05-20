76fef14748a7da1e900e9913355919526dff61db
antdewei <186699613+antdewei@users.noreply.github.com>
2026-05-21 00:21:11 +0800

    add elbv2 streaming regression tests

--- collector/aws/collector/elasticloadbalancing/elasticloadbalancingv2.go
+++ collector/aws/collector/elasticloadbalancing/elasticloadbalancingv2.go
@@ -17,6 +17,7 @@ package elasticloadbalancing
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	types2 "github.com/aws/aws-sdk-go-v2/service/ec2/types"
@@ -29,6 +30,11 @@ import (
 	"github.com/core-sdk/schema"
 	"go.uber.org/zap"
 )
+
+type elbv2API interface {
+	DescribeLoadBalancers(context.Context, *elasticloadbalancingv2.DescribeLoadBalancersInput, ...func(*elasticloadbalancingv2.Options)) (*elasticloadbalancingv2.DescribeLoadBalancersOutput, error)
+	DescribeListeners(context.Context, *elasticloadbalancingv2.DescribeListenersInput, ...func(*elasticloadbalancingv2.Options)) (*elasticloadbalancingv2.DescribeListenersOutput, error)
+}
 
 // GetELBResource returns a  ELB Resource
 // ELB is elasticloadbalancingv2
@@ -83,48 +89,122 @@ func GetELBDetail(ctx context.Context, iService schema.ServiceInterface, res cha
 	elbClient := iService.(*collector.Services).ELB
 	ec2Client := iService.(*collector.Services).EC2
 
-	return forEachELB(ctx, elbClient, func(elb types.LoadBalancer) error {
-		listeners, err := describeELBListenersByLoadBalancerArn(ctx, elbClient, elb.LoadBalancerArn)
-		if err != nil {
-			log.CtxLogger(ctx).Warn("DescribeListeners error", zap.Error(err), zap.String("loadBalancerArn", aws.ToString(elb.LoadBalancerArn)))
-		}
-		res <- ELBDetail{
-			ELB:       elb,
-			Listeners: listeners,
-			VPC: ec2.DescribeVPCDetailsByFilters(ctx, ec2Client, []types2.Filter{
+	return streamELBDetails(
+		ctx,
+		elbClient,
+		res,
+		func(ctx context.Context, elb types.LoadBalancer) []ec2.VPCDetail {
+			if elb.VpcId == nil {
+				return nil
+			}
+			return ec2.DescribeVPCDetailsByFilters(ctx, ec2Client, []types2.Filter{
 				{
 					Name:   aws.String("vpc-id"),
 					Values: []string{*elb.VpcId},
 				},
-			}),
-			SecurityGroups: ec2.DescribeSecurityGroupDetailsByFilters(ctx, ec2Client, []types2.Filter{
+			})
+		},
+		func(ctx context.Context, elb types.LoadBalancer) []ec2.SecurityGroupDetail {
+			return ec2.DescribeSecurityGroupDetailsByFilters(ctx, ec2Client, []types2.Filter{
 				{
 					Name:   aws.String("group-id"),
 					Values: elb.SecurityGroups,
 				},
-			}),
-		}
-		return nil
-	})
+			})
+		},
+	)
 }
 
 func GetELBListenerDetail(ctx context.Context, iService schema.ServiceInterface, res chan<- any) error {
 	elbClient := iService.(*collector.Services).ELB
 
-	return forEachELB(ctx, elbClient, func(elb types.LoadBalancer) error {
-		listeners, err := describeELBListenersByLoadBalancerArn(ctx, elbClient, elb.LoadBalancerArn)
+	return streamELBListeners(ctx, elbClient, res)
+}
+
+func streamELBDetails(
+	ctx context.Context,
+	c elbv2API,
+	res chan<- any,
+	describeVPCDetails func(context.Context, types.LoadBalancer) []ec2.VPCDetail,
+	describeSecurityGroupDetails func(context.Context, types.LoadBalancer) []ec2.SecurityGroupDetail,
+) error {
+	start := time.Now()
+	var processed, listenerCount, listenerErrorCount int
+	log.CtxLogger(ctx).Info("Start streaming ELB details")
+	defer func() {
+		log.CtxLogger(ctx).Info("Finished streaming ELB details",
+			zap.Int("processedLoadBalancers", processed),
+			zap.Int("listeners", listenerCount),
+			zap.Int("listenerErrors", listenerErrorCount),
+			zap.Duration("duration", time.Since(start)))
+	}()
+
+	return forEachELB(ctx, c, func(elb types.LoadBalancer) error {
+		listeners, err := describeELBListenersByLoadBalancerArn(ctx, c, elb.LoadBalancerArn)
 		if err != nil {
+			listenerErrorCount++
 			log.CtxLogger(ctx).Warn("DescribeListeners error", zap.Error(err), zap.String("loadBalancerArn", aws.ToString(elb.LoadBalancerArn)))
+		}
+		detail := ELBDetail{
+			ELB:       elb,
+			Listeners: listeners,
+		}
+		if describeVPCDetails != nil {
+			detail.VPC = describeVPCDetails(ctx, elb)
+		}
+		if describeSecurityGroupDetails != nil {
+			detail.SecurityGroups = describeSecurityGroupDetails(ctx, elb)
+		}
+		res <- detail
+
+		processed++
+		listenerCount += len(listeners)
+		logELBProgress(ctx, "Streaming ELB detail progress", processed, listenerCount, listenerErrorCount)
+		return nil
+	})
+}
+
+func streamELBListeners(ctx context.Context, c elbv2API, res chan<- any) error {
+	start := time.Now()
+	var processed, listenerCount, listenerErrorCount int
+	log.CtxLogger(ctx).Info("Start streaming ELB listeners")
+	defer func() {
+		log.CtxLogger(ctx).Info("Finished streaming ELB listeners",
+			zap.Int("processedLoadBalancers", processed),
+			zap.Int("listeners", listenerCount),
+			zap.Int("listenerErrors", listenerErrorCount),
+			zap.Duration("duration", time.Since(start)))
+	}()
+
+	return forEachELB(ctx, c, func(elb types.LoadBalancer) error {
+		listeners, err := describeELBListenersByLoadBalancerArn(ctx, c, elb.LoadBalancerArn)
+		processed++
+		if err != nil {
+			listenerErrorCount++
+			log.CtxLogger(ctx).Warn("DescribeListeners error", zap.Error(err), zap.String("loadBalancerArn", aws.ToString(elb.LoadBalancerArn)))
+			logELBProgress(ctx, "Streaming ELB listener progress", processed, listenerCount, listenerErrorCount)
 			return nil
 		}
 		for _, listener := range listeners {
 			res <- ELBListenerDetail{Listener: listener}
 		}
+
+		listenerCount += len(listeners)
+		logELBProgress(ctx, "Streaming ELB listener progress", processed, listenerCount, listenerErrorCount)
 		return nil
 	})
 }
 
-func describeELBListenersByLoadBalancerArn(ctx context.Context, c *elasticloadbalancingv2.Client, loadBalancerArn *string) (listeners []types.Listener, err error) {
+func logELBProgress(ctx context.Context, msg string, processed int, listenerCount int, listenerErrorCount int) {
+	if processed == 1 || processed%50 == 0 {
+		log.CtxLogger(ctx).Info(msg,
+			zap.Int("processedLoadBalancers", processed),
+			zap.Int("listeners", listenerCount),
+			zap.Int("listenerErrors", listenerErrorCount))
+	}
+}
+
+func describeELBListenersByLoadBalancerArn(ctx context.Context, c elbv2API, loadBalancerArn *string) (listeners []types.Listener, err error) {
 	if loadBalancerArn == nil {
 		return listeners, nil
 	}
@@ -148,16 +228,23 @@ func describeELBListenersByLoadBalancerArn(ctx context.Context, c *elasticloadba
 	return listeners, nil
 }
 
-func forEachELB(ctx context.Context, c *elasticloadbalancingv2.Client, handle func(types.LoadBalancer) error) error {
+func forEachELB(ctx context.Context, c elbv2API, handle func(types.LoadBalancer) error) error {
 	input := &elasticloadbalancingv2.DescribeLoadBalancersInput{
 		PageSize: aws.Int32(400),
 	}
+	var page, total int
 	for {
 		output, err := c.DescribeLoadBalancers(ctx, input)
 		if err != nil {
 			log.CtxLogger(ctx).Warn("DescribeLoadBalancers error", zap.Error(err))
 			return err
 		}
+		page++
+		total += len(output.LoadBalancers)
+		log.CtxLogger(ctx).Info("DescribeLoadBalancers page received",
+			zap.Int("page", page),
+			zap.Int("pageLoadBalancers", len(output.LoadBalancers)),
+			zap.Int("totalLoadBalancers", total))
 		for _, elb := range output.LoadBalancers {
 			if err := handle(elb); err != nil {
 				return err
